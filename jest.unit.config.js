5fe6c3b4eba940002bf1be53ecb9f1e981a2b995
Ulad Kasach <uladkasach@gmail.com>
2021-07-27 06:45:50 -0400

    fix(tests): *.test.integration -> *.integration.test

--- jest.unit.config.js
+++ jest.unit.config.js
@@ -8,7 +8,7 @@ module.exports = {
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
-  testMatch: ['**/src/**/?(*.)+(spec|test).(js|ts)'],
+  testMatch: ['**/*.test.ts', '!**/*.integration.test.ts'],
   testEnvironment: 'node',
   setupFiles: ['core-js'], // .flat() undefined otherwise; https://stackoverflow.com/a/59285424/3068233
   verbose: true,
